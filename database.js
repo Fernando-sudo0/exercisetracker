e316e998e2c9ccf3b0a1e58c2981029988b9feec
Fernando Gutierrez <fernando.gtz.escarrega@gmail.com>
2022-07-10 18:09:38 -0700

    beta

---
+++ database.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const config = {
+    autoIndex : false, 
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+  }
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sa:Contra123@cluster0.typgrkr.mongodb.net/Exercisetrackerdb";
+
+mongoose.connect(uri, config);
+
+let UsersSchema = new mongoose.Schema({  username: {type : String}});
+
+let ExercisesSchema = new mongoose.Schema ({
+    userId: {type : String, required : true},
+    description: {type : String, required  : true},
+    duration: {type : Number, min : 1, required  : true},
+    date: {type : Date, default : Date.now}})
+
+const UserModel =  mongoose.model('Users', UsersSchema)
+const ExercisesModel = mongoose.model('Exercises', ExercisesSchema);
+
+const createUser= (username, done) => {
+  try{
+    const Users = new UserModel({username});
+
+    Users.save(function(err, data){
+        if(err) return console.log(err);
+        done(null, data)
+    }); 
+
+  }catch(error){
+    console.log(error);
+  }
+}
+// id = 62cb4590b00431b211b986a7
+const createExercises = (userId, description, duration, date, done) => {
+    const Exercises = new ExercisesModel({userId, description, duration, date});
+    Exercises.save(function(err, data){
+      if(err) return console.log(err);
+      done(null, data);
+    })
+  
+}
+const findOneUserById = (userId, done) =>{
+    UserModel.findById(userId, function(err, data) {
+      if(err)return console.log(err)
+      done(null, data)
+    })
+}
+const findExercisesByUser = (userId,limit, startdate, enddate, done) =>{
+ 
+  ExercisesModel.find({userId , date : {$gte : startdate, $lte : enddate}})
+                .sort({date : 'asc'})
+                .limit(limit)
+                .exec( function(err, data){
+                  if(err) return console.log(err);
+                  done(null, data)
+                });
+}
+
+const findUsers = (done) => {
+  UserModel.find({}, function(err, data){
+    if(err) return console.log(err);
+    done(null, data);
+  })
+}
+
+  exports.createUser = createUser;
+  exports.createExercises = createExercises;
+  exports.findUsers = findUsers;
+  exports.findOneUserById = findOneUserById;
+  exports.findExercisesByUser  = findExercisesByUser ;
--- index.js
+++ index.js
@@ -1,18 +1,87 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+const db = require('./database.js')
 
+app.post('/api/users', (req, res) => {
+//create a new user and return a json with username and id
+//each user is different register
+  const {username} = req.body
 
+  db.createUser(username, function(err, data){
+   
+    if(err){
+      return next(err)
+    }
+    if(!data){
+      return next({message: 'Missing call argument'})
+    }
+    return res.json({data})
+  })
+});
+app.get('/api/users', (req, res)=> {
+  db.findUsers(function(err, data){
+    if(err){
+      return next(err)
+    }
+    return res.json({data})
+  })
 
+})
+app.post('/api/users/:_id/exercises', (req, res) => {
+  //create a exersice with an id, and callback with userId and username,
+  const {description, duration, date} = req.body
+  const userId = req.params._id
+  var date2  = date
+
+  if (date == undefined || date == '')
+    date2 = new Date()
+
+  db.findOneUserById(userId, function(err, userdata){
+    if(err){
+      return next(err)
+    }
+    if(!userdata){
+      return next({message: 'User missing'})
+    }
+
+    db.createExercises(userId, description, duration, date2, function(err, data){
+      if(err){
+        return next(err)
+      }
+      return res.json({username : userdata.username, description: data.description, duration : data.duration, date : data.date.toUTCString(), _id : data._id});
+    });
+  })
+})
+
+// http://localhost:3000/api/users/62cb262d2407cf09260143a3/logs?from=2018-01-01&to=2023-07-23&limit=100
+app.get('/api/users/:_id/logs?', (req, res) => {
+
+var startDate = new Date(req.query.from);
+var endDate = new Date (req.query.to);
+
+db.findOneUserById(req.params._id,  function(err, userdata){
+  if(err) return console.log(err)
+    db.findExercisesByUser(req.params._id, req.query.limit, startDate, endDate, function(err, exersicedata){
+      if(err){next(err)}
+      return res.json({  _id : userdata._id, username: userdata.username, to : startDate, from : endDate,count : exersicedata.length , log : exersicedata.map(function(e){  return { description: e.description,duration :  e.duration, date : e.date.toUTCString() }})})
+    })
+  });
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
